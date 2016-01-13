dcadc55ff24ffd1c75f8f5ec61206946ea821071
Edwin Bratini <edwin.bratini@gmail.com>
2016-01-13 05:56:29 +0000

    Fix and add custom message to role inclusion validation

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,5 +2,6 @@ class Role < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
   
-  validates role, inclusion: { in: %[:viewer, :editor, :manager] }
+  validates :role, inclusion: { in: %w[viewer editor manager],
+    message: "%{value} is not a valid role" }
 end
