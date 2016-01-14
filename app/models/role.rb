17398639ca1f2aab5c207ec7941071ce73f209b1
Edwin Bratini <edwin.bratini@gmail.com>
2016-01-14 02:34:10 +0000

    Handle authorization errors gracefully

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,6 +2,10 @@ class Role < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
   
-  validates :role, inclusion: { in: %w[viewer editor manager],
-    message: "%{value} is not a valid role" }
+  def self.available_roles
+    %w[viewer editor manager]
+  end
+  
+  validates :role, inclusion: { in: self.available_roles,
+                                message: "%{value} is not a valid role" }
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe ProjectsController, type: :controller do
     expect(response).to redirect_to(projects_path)
     expect(flash[:alert]).to eq message
   end
+  
+  it 'handles permission errors by redirecting to a safe place' do
+    allow(controller).to receive(:current_user)
+    
+    project = FactoryGirl.create(:project)
+    get :show, id: project
+    
+    message = 'You aren\'t allowed to do that.'
+    expect(response).to redirect_to(root_path)
+    expect(flash[:alert]).to eq message
+  end
 end
