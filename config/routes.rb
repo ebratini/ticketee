156094b054808a50ae1845c9139bb9e19027ed04
Edwin Bratini <edwin.bratini@gmail.com>
2016-04-28 05:05:19 +0000

    Admins can create new states for tickets

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   namespace :admin do
     root 'application#index'
+    
     resources :projects, only: [:new, :create, :destroy]
     resources :users do
       member do
         patch :archive
       end
     end
+    resources :states, only: [:index, :new, :create]
   end
 
   devise_for :users
