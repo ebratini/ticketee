2703266cf9ce00e8054a2a3d14d1f473bf958316
Edwin Bratini <edwin.bratini@gmail.com>
2016-09-04 00:32:25 +0000

    Give more details about the ticket in email body

--- spec/mailers/comment_notifier_spec.rb
+++ spec/mailers/comment_notifier_spec.rb
@@ -1,4 +1,29 @@
 require "rails_helper"
 
 RSpec.describe CommentNotifier, type: :mailer do
+  describe 'created' do
+    let(:project) { FactoryGirl.create(:project) }
+    let(:ticket_owner) { FactoryGirl.create(:user) }
+    let(:ticket) do
+      FactoryGirl.create(:ticket, project: project, author: ticket_owner)
+    end
+    
+    let(:commenter) { FactoryGirl.create(:user) }
+    let(:comment) do
+      Comment.new(ticket: ticket, author: commenter, text: 'Test comment')
+    end
+    
+    let(:email) { CommentNotifier.created(comment, ticket_owner) }
+    
+    it 'sends out an email notification about a new comment' do
+      title = "#{ticket.name} for #{project.name} has been updated."
+      email_body = email.body.to_s
+      
+      expect(email.to).to include ticket_owner.email
+      expect(email_body).to include title
+      expect(email_body).to include "#{commenter.email} wrote:"
+      expect(email_body).to include comment.text
+    end
+    
+  end
 end
