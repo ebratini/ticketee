75f8c0f7772ec1a5629a38a5841a3acf3059fe79
Edwin Bratini <edwin.bratini@gmail.com>
2016-09-08 06:22:45 +0000

    Fix prod mailgun settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,10 +68,10 @@ Rails.application.configure do
   
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    port:           ENV[MAILGUN_SMTP_PORT],
-    address:        ENV[MAILGUN_SMTP_SERVER],
-    user_name:      ENV[MAILGUN_SMTP_LOGIN],
-    password:       ENV[MAILGUN_SMTP_PASSWORD],
+    port:           ENV['MAILGUN_SMTP_PORT'],
+    address:        ENV['MAILGUN_SMTP_SERVER'],
+    user_name:      ENV['MAILGUN_SMTP_LOGIN'],
+    password:       ENV['MAILGUN_SMTP_PASSWORD'],
     domain:         host,
     authentication: :plain
   }
